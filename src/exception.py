11146a524cf1792444b6c4a6f0d30580c5fcf3bb
Soumil007 <samuelroy41@gmail.com>
2023-04-07 17:04:19 +0530

    Testing exceptions.py

--- src/exception.py
+++ src/exception.py
@@ -4,9 +4,14 @@ import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
+
+    # file_name_1 = exc_tb.tb_frame.f_code.co_filename
+
     error_message = 'Error occured in python script name [{0}] line number [{1}] error message [{2}]'.format(
         file_name,exc_tb.tb_lineno,str(error)
     )
+
+    # print("Error_Detail - > ", file_name_1)
 
     return error_message
 
